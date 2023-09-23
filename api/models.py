52948f30b59a1aaf2c824f35076fe15f7129ecf2
Richenel2 <yvanprogramme@gmail.com>
2023-09-23 13:31:59 +0100

    resolve

--- api/models.py
+++ api/models.py
@@ -10,10 +10,9 @@ class Domaine(models.Model):
         base_field=models.CharField(max_length=32),
         size=4,
         max_length=(4*33)
-
     )
     img = models.URLField(max_length=100000,)
-    
+
     def __str__(self) -> str:
         return self.name
     fee = models.IntegerField()
@@ -29,9 +28,9 @@ class Ecole(models.Model):
     img = models.URLField()
     fee = models.IntegerField()
     departments = models.ManyToManyField(Domaine)
+
     def __str__(self) -> str:
         return self.name
-
 
 
 # class Enseigne(models.Model):
@@ -39,12 +38,12 @@ class Ecole(models.Model):
 #     ecole = models.ForeignKey(Ecole, on_delete=models.CASCADE)
 #     year = models.IntegerField()
 #     fee = models.IntegerField()
-50
 
 class Metier (models.Model):
     name = models.CharField(max_length=255)
-    salaire = models.IntegerField()
-    description = models.TextField()
+    fee = models.IntegerField()
+    desc = models.TextField()
+    rating = models.FloatField(default=4.0)
     qualite = ListCharField(
         base_field=models.CharField(max_length=32),
         size=4,
@@ -52,6 +51,7 @@ class Metier (models.Model):
     )
     img = models.URLField(max_length=10000,)
     departments = models.ManyToManyField(Domaine)
+
     def __str__(self) -> str:
         return self.name
 
@@ -60,6 +60,7 @@ class Reponse (models.Model):
     answer = models.CharField(max_length=255)
     domaines = models.ManyToManyField(Domaine)
     point = models.IntegerField(default=10)
+
     def __str__(self) -> str:
         return self.reponse
 
@@ -67,5 +68,6 @@ class Reponse (models.Model):
 class Question (models.Model):
     question = models.TextField()
     answers = models.ManyToManyField(Reponse)
+
     def __str__(self) -> str:
         return self.question
