12968c94497dbec6dcdb2a79c080743d353bfd12
Richenel2 <yvanprogramme@gmail.com>
2023-09-23 12:17:13 +0100

    resolve

--- lugar_api/settings.py
+++ lugar_api/settings.py
@@ -62,7 +62,7 @@ REST_FRAMEWORK = {
     'PAGE_SIZE': 2, }
 
 CORS_ALLOWED_ORIGINS = [
-    'kayra.alwaysdata.net','127.0.0.1'
+    'kayra.alwaysdata.net'
 ]
 CORS_ALLOW_ALL_ORIGINS = True
 TEMPLATES = [
