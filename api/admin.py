92b132368f58d1d3ebae9a8fed255f90c40b13cb
Richenel2 <yvanprogramme@gmail.com>
2023-09-23 12:14:30 +0100

    resolve

--- api/admin.py
+++ api/admin.py
@@ -1,11 +1,10 @@
 from django.contrib import admin
-from .models import Enseigne,Ecole,Domaine,Metier,Question,Reponse
+from .models import Ecole,Domaine,Metier,Question,Reponse
 
 # Register your models here.
 
 admin.site.register(Ecole)
 admin.site.register(Domaine)
-admin.site.register(Enseigne)
 admin.site.register(Metier)
 admin.site.register(Question)
 admin.site.register(Reponse)
