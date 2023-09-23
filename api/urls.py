54895d6b57a2b4f229685c0f6539511e9dccafe2
Richenel2 <yvanprogramme@gmail.com>
2023-09-23 12:15:21 +0100

    resolve

--- api/urls.py
+++ api/urls.py
@@ -1,12 +1,12 @@
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 
-from api.views import (DomaineViewSet, EcoleViewSet, EnseigneViewSet,
+from api.views import (DomaineViewSet, EcoleViewSet,
                        MetierViewSet, QuestionViewSet, ReponseViewSet)
 
 router = DefaultRouter()
 router.register(r'ecole', EcoleViewSet, basename='ecole')
-router.register(r'enseigne', EnseigneViewSet, basename='enseigne')
+# router.register(r'enseigne', EnseigneViewSet, basename='enseigne')
 router.register(r'reponse', ReponseViewSet, basename='reponse')
 router.register(r'question', QuestionViewSet, basename='question')
 router.register(r'domaine', DomaineViewSet, basename='domaine')
